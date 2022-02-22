0e1d1465051d5abc71fb5fd96957cd3ede349ed2
SUTHINZARNWE-PC\SuThinzarNwe <scm.suthinzarnwe@gmail.com>
2022-02-22 16:05:09 +0630

    [main]java exercises added

---
+++ CheckOddEven.java
@@ -0,0 +1,12 @@
+public class CheckOddEven {
+    public static void main(String[] args) {
+        int num=50;
+        System.out.println("The number is " + num);
+
+        if(num % 2 == 0){
+            System.out.println("This number is even number");
+        } else {
+            System.out.println("This number is odd number");
+        }
+    }
+}
